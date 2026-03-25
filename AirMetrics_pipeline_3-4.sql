a9c361f8db90fe2a92d96ae046b7f9418f77465f
Nikolai Polovnikov <82658563+Robenxorst@users.noreply.github.com>
2026-03-25 17:02:40 +0300

    Update stage 3-4

--- AirMetrics_pipeline_3-4.sql
+++ AirMetrics_pipeline_3-4.sql
@@ -1,10 +1,8 @@
--- Задача номер 3-4. 
+-- Этапы 3-4.
 -- Используем кластер sharded, так как предполагаем, что данные огромные.
 
 -- данные о билетах
 SHOW CREATE TABLE clickhouse_data.ticket_flights;
--- посадочные талоны(пассажиры, которые сели в самолет)
-
 select * from clickhouse_data.ticket_flights limit 10;
 
 -- локальная таблица для сырых данных
@@ -24,7 +22,6 @@ ENGINE = ReplicatedMergeTree(
 ORDER BY (flight_no, flight_id);
 
 -- поверх локальной таблицы создаем Distributed
--- таблицу(по сути еще один движок);
 CREATE TABLE ticket_flights_raw ON CLUSTER 'sharded' AS ticket_flights_raw_local
 ENGINE = Distributed(
     'sharded', 
@@ -32,7 +29,6 @@ ENGINE = Distributed(
     'ticket_flights_raw_local', 
     cityHash64(flight_no)
 );
-
 
 -- создаем широкую реплицированную таблицу(ticket_flights + flights)
 create table ticket_flights_wide_local on cluster 'sharded'
@@ -65,7 +61,6 @@ ENGINE = ReplicatedMergeTree(
 )
 partition by toYYYYMM(departure_date)
 ORDER BY (flight_no, departure_date, ticket_no);
--- Успешное создание таблицы!!!
 
 -- Создаем Distributed таблицу для управления шардами кластера;
 CREATE TABLE ticket_flights_wide ON CLUSTER 'sharded' AS ticket_flights_wide_local
@@ -75,15 +70,12 @@ ENGINE = Distributed(
     'ticket_flights_wide_local', 
     cityHash64(flight_no)
 );
--- Готово!!
 
 -- Удаляем старую таблицу
-drop table ticket_flights_wide ON CLUSTER 'sharded';
+-- drop table ticket_flights_wide ON CLUSTER 'sharded';
 
 
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! --
 -- Создаем материализованное представление 
-
 CREATE MATERIALIZED VIEW ticket_flights_raw_to_wide_mv on cluster 'sharded'
 to ticket_flights_wide_local 
 as 
@@ -110,31 +102,19 @@ select
 -- mv будет реагировать на изменения в первой таблице, 
 -- указанной в from
 from ticket_flights_raw_local tr
--- лучше всего использовать 
--- ANY LEFT JOIN для устранения дубликатов
+
 ANY LEFT JOIN flights f
 ON tr.flight_id = f.flight_id;
--- И это правильный ответ!!! Мы получили максимум баллов
 
 -- удаляем нашу таблицу:
 -- drop table ticket_flights_raw_to_wide_mv on cluster 'sharded';
 
--- Вставим много строчек в таблицу с сырыми данными ticket_flights_raw 
+-- Вставим строки в таблицу с сырыми данными ticket_flights_raw 
 -- (через Distributed() , так как имеем дело с шардированным кластером)
 INSERT INTO ticket_flights_raw
 select * from clickhouse_data.ticket_flights;
 
--- Проверим сработало ли наше материальное представление
-
--- обращение к Distributed
-select * from ticket_flights_raw;
-
--- Обращение к локальной таблице хоста
-select * from ticket_flights_raw_local;
--- похоже, что шардирование не работает! Все данные сохраняются на одном хосте, а должны делиться пополам
--- (мы и используем кластер sharded)
-
--- материальное представление при этом корректно заполняет широкую таблицу
+-- материальное представление корректно заполняет широкую таблицу
 select * from ticket_flights_wide_local;
 
 -- удаляем все старые данные(затем обновляем)
